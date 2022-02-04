df1704cffbd7bf17c3c4e5061a8d121aba52c7ed
Lord/Savage45 <98854930+lordSavage263@users.noreply.github.com>
2022-02-05 00:42:37 +0200

    Update Mods.ts

--- src/commands/General/Mods.ts
+++ src/commands/General/Mods.ts
@@ -17,14 +17,14 @@ export default class Command extends BaseCommand {
     run = async (M: ISimplifiedMessage): Promise<void> => {
         if (!this.client.config.mods || !this.client.config.mods[0]) return void M.reply('*[UNMODERATED]*')
         const filteredMap = this.client.config.mods.map((mod) => this.client.getContact(mod)).filter((user) => user)
-        let text = '🍥 *Moderators* 🍥\n\n'
+        let text = '🌺 *My Boss* 🌺\n\n'
         filteredMap.forEach(
             (user, index) =>
-                (text += `#${index + 1}\n⚡ *Username: ${
+                (text += `#${index + 1}\n❤️ *Username: ${
                     user.notify || user.vname || user.name || 'null'
                 }*\n🍀 *Contact: https://wa.me/+${user?.jid?.split('@')[0]}*\n\n`)
         )
-        text += `\n⚡ Yotsuba ⚡ `
+        text += `\n❤️ Elaina ❤️ `
         return void M.reply(text)
     }
 }
