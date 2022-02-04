8f49ec75e1814983d4a2aafae3dcdefc49693689
Lord/Savage45 <98854930+lordSavage263@users.noreply.github.com>
2022-02-05 00:11:01 +0200

    Update Sticker.ts

--- src/commands/Utils/Sticker.ts
+++ src/commands/Utils/Sticker.ts
@@ -80,8 +80,8 @@ export default class Command extends BaseCommand {
 					})();
 					return {
 						categories,
-						pack: pack[1] || "⚡ Here you go ",
-						author: pack[2] || "Yotsuba ⚡",
+						pack: pack[1] || "❤️Hey it's me ",
+						author: pack[2] || "Lord Savage 🕊️ ",
 						quality,
 						type: StickerTypes[
 							parsedArgs.flags.includes("--crop") ||
