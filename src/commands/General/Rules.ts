0ccdc98d996b71d8dfdbcebe5f6920a61bfde9b0
Lord/Savage45 <98854930+lordSavage263@users.noreply.github.com>
2022-02-05 00:55:49 +0200

    Update Rules.ts

--- src/commands/General/Rules.ts
+++ src/commands/General/Rules.ts
@@ -19,8 +19,8 @@ export default class Command extends BaseCommand {
         let chitoge = n[Math.floor(Math.random() * n.length)]
         return void this.client.sendMessage(M.from, { url: chitoge }, MessageType.video, {
             mimetype: Mimetype.gif,
-            caption: `_*----🎀[Rule]🎀----*_\n\n❌*DONT ASK FOR THE SCRIPT*🚫\n*>>>* use $support to get the Official group link in your dm\n*--->*  If you want to chat with Star you can use *Don't chat in private bot can't chat in Pm\n*--->* If you want to add Yotsuba in your group the contact the owner by *$owner/$mods* \n*--->* Dont use wrong command, use the command given in the *help list* \n*--->* Dont spam the bot with commands if the bot is not responding, its means the bot maybe offline or facing internet issue. \n*--->* Dont Dm the bot \n\n*IF YOU DONT FOLLOW THE RULES THEN YOU WILL BE BAN SOON* 🚫  ` }
+            caption: `_*----❤️[Bot Rules]❤️----*_\n\n❌*PLEASE DONT ASK FOR THE BOTSCRIPT*🙃\n*>>>* use #support to get the Official group link in your dm\n*--->*  If you want to chat with Elaina you can use *Don't chat in groups bot can only chat in dm\n*--->* If you want to add Elaina in your group the contact then contact owner by *#owner/$mods* \n*--->* Dont call the bot if you call you will be blocked with immediate effect dont  use wrong command, use the command given in the *help list* \n*--->* Dont spam the bot with commands if the bot is not responding, it means the bot maybe offline or facing internet issue. \n*--->* Dont Dm the bot \n\n*IF YOU DONT FOLLOW THE RULES THEN YOU WILL BE BANNED * 😑  ` }
         )
-    }
+    } 
 }
    
